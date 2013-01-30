7889f67be7ea3bb10efe0843bea3df97bf9c8bec
Steven Caamano <diceroll123@gmail.com>
2013-01-30 09:16:02 -0500

    Removes Flattr

--- javascript/ui.js
+++ javascript/ui.js
@@ -93,14 +93,6 @@
       aboutInit = true;
 
       (function() {
-        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'https://api.flattr.com/js/0.6/load.js?mode=auto';
-        t.parentNode.insertBefore(s, t);
-      })();
-
-      (function() {
         var twitterScriptTag = document.createElement('script');
         twitterScriptTag.type = 'text/javascript';
         twitterScriptTag.async = true;
--- widgets/tutorial/tutorial.js
+++ widgets/tutorial/tutorial.js
@@ -26,7 +26,6 @@ $("#next,#back").live("click", function(){
 $(window).load(function() {
   loadPlusOneScript();
   loadTwitterScript();
-  loadFlattrScript();
 });
 
 function loadPlusOneScript() {
@@ -44,13 +43,4 @@ function loadTwitterScript() {
   twitterScriptTag.src = 'https://platform.twitter.com/widgets.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(twitterScriptTag, s);
-}
-
-function loadFlattrScript() {
-  var s = document.createElement('script'),
-  t = document.getElementsByTagName('script')[0];
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = 'https://api.flattr.com/js/0.6/load.js?mode=auto&popout=0&https=1';
-  t.parentNode.insertBefore(s, t);
 }
