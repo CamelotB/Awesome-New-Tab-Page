57d7543bf195e49ce49a636609fbf3d9feb32498
Steven Caamano <diceroll123@gmail.com>
2013-01-31 08:33:17 -0500

    Removes Smooth Scrolling

--- javascript/hscroll.js
+++ javascript/hscroll.js
@@ -1,86 +1,46 @@
-/* START :: Horizontal Scrolling Smarts */
+/* START :: Horizontal Scrolling */
 
   var hscroll = true;
-  $("body > .ui-2, body > #recently-closed-tabs-menu").live({
+  $(document).on({
     mouseleave: function() {
       hscroll = true;
     },
     mouseenter: function() {
       hscroll = false;
     }
-  });
+  }, "body > .ui-2, body > #recently-closed-tabs-menu");
 
-  /* END :: Horizontal Scrolling Smarts */
+  function scrollHorizontal(event) {
+    var delta = 0;
 
-/* Horizontal Scrolling by Paul Bennet
- *
- * Copyright 2008 Paul Bennett - http://paulicio.us
- * Scroller.js
- * Captures mouse wheel events and runs the ScrollSmoothly
- * function based on their output.
- * Aims to aid usability by allowing the user to scroll the
- * page horizontally smoothly using only their mousewheel.
- * Mousewheel event capture by Adomas Paltanavičius at http://adomas.org/
- */
+    if ( !event )
+      event = window.event;
 
-  function handle(delta) {
-    if (delta < 0)
-      ScrollSmoothly(25, 3, "right");
-    else if (delta > 0)
-      ScrollSmoothly(25, 3, "left");
-  }
+    if ( event.originalEvent )
+      event = event.originalEvent;
 
-  function wheel(event){
-    if(hscroll === false) {
+    if ( hscroll === false ) {
+      if ( event.preventDefault )
+        event.preventDefault();
+
+      event.returnValue = false;
       return;
     }
-    var delta = 0;
-    if (!event)
-      event = window.event;
-    if (event.wheelDelta) {
+
+    if ( event.wheelDelta ) {
       delta = event.wheelDelta/120;
-      if (window.opera)
-        delta = -delta;
-    } else if (event.detail) {
-      delta = -event.detail/3;
     }
-    if (delta)
-      handle(delta);
-    if (event.preventDefault)
+
+    if (delta < 0)
+      window.scrollBy(150, 0);
+    else if (delta > 0)
+      window.scrollBy(-150, 0);
+
+    if ( event.preventDefault )
       event.preventDefault();
+
     event.returnValue = false;
   }
+  $(document).live("mousewheel", scrollHorizontal);
 
-  var repeatCount = 0;
-
-  function ScrollSmoothly(scrollPos, repeatTimes, direction) {
-    if(repeatCount < repeatTimes) {
-      if(direction === "right")
-        window.scrollBy(scrollPos, 0);
-      else
-        window.scrollBy(-scrollPos, 0);
-    } else {
-        repeatCount = 0;
-        clearTimeout(cTimeout);
-        return;
-    }
-
-    repeatCount++;
-    cTimeout = setTimeout(function() {
-      ScrollSmoothly(scrollPos, repeatTimes, direction);
-    }, 15);
-
-  }
-
-  /* Initialization code. */
-
-  if (window.addEventListener) {
-    window.addEventListener('DOMMouseScroll', wheel, false);
-  }
-
-  window.onmousewheel = document.onmousewheel = wheel;
-
-  $(document).live("mousewheel", function(e) {
-    wheel(e.originalEvent);
-  });
-
+  /* END :: Horizontal Scrolling */
